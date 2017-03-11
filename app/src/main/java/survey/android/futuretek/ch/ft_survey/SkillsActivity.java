0947f15fc2523a6f11527e5938ba58bb21c55880
Monica Rocha <monica.rocha@arcww.pt>
2017-03-11 16:50:05 +0000

    finished challenge

---
+++ app/src/main/java/survey/android/futuretek/ch/ft_survey/SkillsActivity.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (C) futuretek AG 2016
+ * All Rights Reserved
+ *
+ * @author Artan Veliju
+ */
+package survey.android.futuretek.ch.ft_survey;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SkillsActivity extends BaseActivity {
+    private Button btn_add;
+    private ListView listview;
+    public List<String> _productlist = new ArrayList<String>();
+    private ListAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+
+        listview = (ListView) findViewById(R.id.listView);
+        View mainTextView = findViewById(R.id.textLayout);
+        mainTextView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((ViewGroup)findViewById(R.id.textLayout)).removeAllViews();
+        List<String> textArray = new ArrayList<>(1);
+        textArray.add("Please add a developer skill");
+        animateText(textArray);
+        _productlist.clear();
+        _productlist = getDatabase().getAllSkills();
+        adapter = new ListAdapter(this);
+        listview.setAdapter(adapter);
+        
+        //if selected skill value is 'None of the above', I'll request the new skill
+        animateText(textArray, new AnimationListDone() {
+            public void done() {
+                requestSkill();
+            }
+        });
+    }
+
+    private void requestSkill(){
+
+        openInputDialog(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText inputSkill = ((EditText) v.findViewById(R.id.userInput));
+                try {
+                    skills = getDatabase().get("skills");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (inputSkill == null || inputSkill.isEmpty()) {
+                    List<String> textArray = new ArrayList<String>(1);
+                    textArray.add("Please add a skill");
+                    animateText(textArray, new AnimationListDone() {
+                    });
+                } else {
+                    insertSkill(inputSkill);
+                }
+            }
+        });
+    }
+
+    private class ListAdapter extends BaseAdapter {
+        LayoutInflater inflater;
+        ViewHolder viewHolder;
+
+        public ListAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public int getCount() {
+            return _productlist.size();
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.list_row, null);
+                viewHolder = new ViewHolder();
+                viewHolder.textView = (TextView) convertView.findViewById(R.id.textView1);
+                viewHolder.delBtn = (Button) convertView.findViewById(R.id.deleteBtn);
+                viewHolder.delBtn.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        ViewGroup row = ((ViewGroup)v.getParent());
+                        String id = ((TextView)row.findViewById(R.id.textView1)).getText().toString();
+                        getDatabase().deleteSkill(id);
+                        _productlist.remove(id);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.textView.setText(_productlist.get(position));
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        TextView textView;
+        Button delBtn;
+
+    }
+
+    private void insertSkill(String skill){
+        try {
+            getDatabase().putSkill(skill);
+            _productlist = getDatabase().getAllSkills();
+            adapter.notifyDataSetChanged();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
